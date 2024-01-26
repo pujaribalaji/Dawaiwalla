1bbe7f47e700cca78f4202875eab6007f18f3d85
pujaribalaji <pujaribalaji152@gmail.com>
2024-01-26 17:45:41 +0530

    commit

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom";
 import useCategory from "../hooks/useCategory";
 import Layout from "../components/Layout/Layout";
+
 const Categories = () => {
   const allCategory = useCategory();
+
+  // Check if allCategory is undefined or null
+  if (!allCategory) {
+    // You can add a loading state or any other fallback UI here
+    return <div>Loading...</div>;
+  }
+
   return (
     <Layout title={"All Category - Dawaiwalla"}>
       <div className="container" style={{ marginTop: "100px" }}>
