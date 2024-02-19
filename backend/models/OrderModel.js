1e15b286ade209a6165c2235f97a9fad0425b45a
pujaribalaji <pujaribalaji152@gmail.com>
2024-02-19 15:41:55 +0530

    commit

--- backend/models/OrderModel.js
+++ backend/models/OrderModel.js
@@ -20,7 +20,7 @@ const orderSchema = new mongoose.Schema({
   status: {
     type: String,
     enum: ["Pending", "Completed", "Cancelled"], // Customize based on your needs
-    default: "Completed",
+    default: "Pending",
   },
   user: {
     type: String,
